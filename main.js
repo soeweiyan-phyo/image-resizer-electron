dbc30bded3f68ff71dad1269e1a3910714fc91e3
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2024-03-06 21:02:25 +1100

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron/main");
+const { create } = require("domain");
+
+const isDev = process.env.NODE_ENV !== "production";
+const isMac = process.platform === "darwin";
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: isDev ? 1000 : 500,
+    height: 600,
+  });
+
+  // Open devtools if in Dev mode
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
