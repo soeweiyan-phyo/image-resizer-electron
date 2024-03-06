f4960642a9b625a081931352c0f6f909864f4b60
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2024-03-07 10:58:15 +1100

    Add menu and about item

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron/main");
-const { create } = require("domain");
+const { app, BrowserWindow, Menu } = require("electron/main");
 
 const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
@@ -20,8 +19,24 @@ function createMainWindow() {
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+// Create about window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+// App is ready
 app.whenReady().then(() => {
   createMainWindow();
+
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -29,6 +44,39 @@ app.whenReady().then(() => {
     }
   });
 });
+
+// Menu template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: "fileMenu",
+  },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
 
 app.on("window-all-closed", () => {
   if (!isMac) {
